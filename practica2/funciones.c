868bd6043d2da3ef1ed1f21c387a143f92f66fd0
Bania1 <115702480+Bania1@users.noreply.github.com>
2023-10-21 00:14:53 +0200

    first commit

---
+++ practica2/funciones.c
@@ -0,0 +1,216 @@
+#include "funciones.h"
+
+//-----------------Funciones de usuario----------------
+//
+bool existeUsuario(char * usuario)
+{
+    FILE *fich;
+    char linea[100];
+    char *nombre;
+    char *password;
+    fich = fopen("usuarios.txt", "r");
+    if (fich == NULL)
+    {
+        printf("Error al abrir el fichero\n");
+        return false;
+    }
+
+    while (fgets(linea,100,fich))
+    {
+        nombre = strtok(linea, ","); 
+        password = strtok(linea, ","); 
+        if (strcmp(nombre, usuario) == 0)
+        {
+            fclose(fich);
+            return true;
+        }
+    }
+    fclose(fich);
+    return false;
+}
+
+bool passwordCorrecta(char * usuario, char * password)
+{
+    FILE *fich;
+    char linea[100];
+    char *nombre;
+    char *passwordFichero;
+    fich = fopen("usuarios.txt", "r");
+    if (fich == NULL)
+    {
+        printf("Error al abrir el fichero\n");
+        return false;
+    }
+
+    while (fgets(linea,100,fich))
+    {
+        char * resto;
+        
+        nombre = strtok_r(linea, ",", &resto); 
+        passwordFichero = strtok(resto, ","); 
+        
+        if (strcmp(nombre, usuario) == 0)
+        {
+            if (strcmp(passwordFichero, password) == 0)
+            {
+                fclose(fich);
+                return true;
+            }
+        }
+    }
+    fclose(fich);
+    return false;
+}
+
+//Funcion que controla que no se repitan los usuarios
+//True si se repite
+bool comprobar_usuario(struct jugador * jugadores, int tam, int socket, char * nombre)
+{
+    for(int i=0; i<tam; i++)
+    {
+        if(jugadores[i].estado == CONECTADO && jugadores[i].socket != socket)
+        {
+            if(strcmp(jugadores[i].nombre, nombre) == 0)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Me falta hacer la funcion que pida el nombre del usuario que lo introduce
+
+// Me falta la funcion de registar usuario
+
+
+//-----------------Funciones partidas-------------------
+//
+void inicializar_vector_partidas(struct partida * partidas, int tam)
+{
+    for (size_t i = 0; i < tam; i++)
+    {
+        partidas[i].socket1 = NO_CONECTADO;
+        partidas[i].socket2 = NO_CONECTADO;
+
+        partidas[i].estado = VACIA;
+    }
+}
+
+bool comprobar_partidas(struct partida * partidas, int tam)
+{
+    for(size_t i = 0; i < tam; i++)
+    {
+        if(partidas[i].estado == VACIA  || partidas[i].estado == SEMILLENA)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Esta funcion busca las partidas semillena o iniciada
+//false --> jugador solo
+//true --> jugador con otro jugador
+bool buscarPartida(struct jugador * user, struct partida * partidas, int socket, int tam)
+{
+    for(size_t i = 0; i < tam; i++)
+    {
+        if(partidas[i].estado == SEMILLENA)
+        {
+            partidas[i].socket2 = socket;
+            partidas[i].estado = INICIADA;
+            return true;
+        }
+    }  
+    
+    for(size_t i = 0; i < tam; i++)
+    {
+        if(partidas[i].estado == VACIA)
+        {
+            partidas[i].socket1 = socket;
+            partidas[i].estado = SEMILLENA;
+            return false;
+        }
+    }      
+
+    return false;
+}
+
+// void actualizarEstado_partida(struct partida * partidas, struct jugador * jugadores, int tam)
+// {
+//     for(size_t i = 0; i < tam; i++)
+//     {
+//         if(partidas[i].estado == VACIA)
+//         {
+//             if(jugadores[buscarSocket(jugadores, tam, partidas[i].socket1)].estado == NO_CONECTADO || jugadores[buscarSocket(jugadores, tam, partidas[i].socket2)].estado == NO_CONECTADO)
+//             {
+//                 partidas[i].estado = NO_CONECTADO;
+//                 jugadores[buscarSocket(jugadores, tam, partidas[i].socket1)].estado = NO_CONECTADO;
+//                 jugadores[buscarSocket(jugadores, tam, partidas[i].socket2)].estado = NO_CONECTADO;
+//             }
+//         }
+//     }
+// }
+
+//Ver si es necesario
+// void crearPartida(struct partida * partidas, struct jugador * jugadores, int socket, int tam)
+// {   
+//     for(size_t i = 0; i < tam; i++)
+//     {
+//         if(partidas[i].estado == NO_CONECTADO)
+//         {
+//             partidas[i].socket1 = socket;
+//             partidas[i].estado = ESPERANDO;
+//             jugadores[buscarSocket(jugadores, tam, socket)].estado = ESPERANDO;
+//             return;
+//         }
+//     }
+// }
+
+//-----------------Funciones de servidor----------------
+//
+void inicializar_vector_jugadores(struct jugador * jugadores, int tam)
+{
+    for(int i=0; i<tam; i++)
+    {
+        jugadores[i].socket = -1;
+        jugadores[i].estado = NO_CONECTADO;
+    }
+}
+
+void guardarNuevoJugador(struct jugador * jugadores,int tam, int socket)
+{
+    for(int i=0; i<tam; i++)
+    {
+        if(jugadores[i].estado == NO_CONECTADO)
+        {
+            jugadores[i].socket = socket;
+            jugadores[i].estado = CONECTADO;
+        }
+    }
+}
+
+int buscarSocket(struct jugador * jugadores,int tam, int socket)
+{
+    for(int i=0; i<tam; i++)
+    {
+        if(jugadores[i].socket == socket)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int buscarSocketDisponible(struct jugador * jugadores, int tam)
+{
+    for(int i=0; i<tam; i++)
+    {
+        if(jugadores[i].estado == ESPERANDO)
+        {
+            return jugadores[i].socket;
+        }
+    }
+    return -1;
+}
---
+++ practica2/funciones.h
@@ -0,0 +1,109 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+//-------------Librerias----------------
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+
+//-------------Constantes----------------
+//
+
+//Estados de los jugadores
+#define NO_CONECTADO -1
+#define CONECTADO 1
+#define USUARIO_ENCONTRADO 2
+#define LOGUEADO 3
+#define ESPERANDO 4
+#define JUGANDO 5
+
+//Estados de las partidas
+#define VACIA 6 //partida vacia
+#define SEMILLENA 7 //1 jugador en partida
+#define INICIADA 8 //2 jugadores en partida
+#define TERMINADA 9 //partida terminada
+
+//Mensajes y tamaños
+#define MSG_SIZE 250
+#define MAX_CLIENTS 30
+#define MAX_MATCHES 10
+
+//-------------Estructuras----------------
+//
+struct jugador
+{
+    int socket;
+    int estado;
+    char nombre_user[50];
+    char nombre[50];
+    //y más cosas
+    //a lo mejor un vector de partidos
+    //a lo mejor un contador para los disparos
+};
+
+struct partida
+{
+    int socket1;
+    int socket2;
+    int estado;
+    int turno;
+
+    //y más cosas?
+
+    
+};
+
+
+//-------------Funciones de usuario----------------
+//
+//Funcion que comprueba si existe usuario y controla que no se repitan los usuarios
+bool existeUsuario(char *usuario);
+
+bool passwordCorrecta(char * usuario, char * password);
+
+//Funcion que comprueba si se repite un usuario
+bool comprobar_usuario(struct jugador * jugadores, int tam, int socket, char * nombre);
+
+//bool registrarUsuario(char * usuario, char * password);
+
+//-------------Funciones partidas------------------
+//
+void inicializar_vector_partidas(struct partida * partidas, int tam);
+
+//Esta funcion comprueba la peticion de los jugadores para iniciar partida
+//Devuelve true --> Partida disponible
+//Devuelve false --> Partida no disponible
+bool comprobar_partidas(struct partida * partidas, int tam);
+
+void crearPartida(struct partida * partidas, struct jugador * jugadores, int socket, int tam);
+
+bool buscarPartida(struct jugador *user, struct partida *partidas, int socket, int tam);
+
+void actualizarEstado_partida(struct partida * partidas, struct jugador * jugadores, int tam);
+
+
+
+//-------------Funciones de servidor----------------
+//
+void inicializar_vector_jugadores(struct jugador * jugadores, int tam);
+
+void guardarNuevoJugador(struct jugador * jugadores,int tam, int socket);
+
+int buscarSocket(struct jugador * jugadores,int tam, int socket);
+
+int buscarSocketDisponible(struct jugador * jugadores, int tam);
+
+//Hacer una funcion de buscar usuario
+
+//La de registrar usuarios para el final
+
+#endif
