5a7575e1e7bc6097b0cbff4dfca63d6e9773f2de
Bania1 <denicoangel@gmail.com>
2023-10-27 17:29:33 +0200

    Co-authored-by: Francisco Javier Fernandez <fjavifp4@users.noreply.github.com>

--- Battleship-main/practica2/tablero.c
+++ Battleship-main/practica2/tablero.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define FILAS 10
 #define COLUMNAS 10
@@ -9,15 +10,15 @@
 // Estructura para representar una cuadrícula
 typedef struct {
     char tablero[FILAS][COLUMNAS];
-    int coordenada[FILAS][COLUMNAS];
+    int idBarco[FILAS][COLUMNAS];
 } Cuadricula;
 
 // Estructura para representar un barco
 typedef struct {
     int tamano;
     char simbolo;
-    int salud; // Nueva propiedad para rastrear la salud del barco                                                                                                                                                                                                                                                                                                                                                                                                 
-    Cuadricula * coordenada;
+    int salud; // Nueva propiedad para rastrear la salud del barco   
+    int idBarco;                                                                                                                                                                                                                                                                                                                                                                                              
 } Barco;
 
 // Estructura para representar las estadísticas de un jugador
@@ -105,17 +106,15 @@ int colocarBarco(Cuadricula* cuadricula, Barco* barco) {
     if (orientacion == 0) {
         for (int i = 0; i < barco->tamano; i++) {
             cuadricula->tablero[fila][columna + i] = barco->simbolo;
-            
-            //guardar la coordenada del barco
-            //barco->id = ;
-
+            cuadricula->idBarco[fila][columna+1]=barco->idBarco;
         }
     } else {
         for (int i = 0; i < barco->tamano; i++) {
             cuadricula->tablero[fila + i][columna] = barco->simbolo;
+            cuadricula->idBarco[fila+1][columna]=barco->idBarco;
         }
     }
-    barco->coordenada->coordenada[fila][columna] = fila,columna; //{fila,columna}
+    
     // Inicializar la salud del barco
     barco->salud = barco->tamano;
 
@@ -125,7 +124,7 @@ int colocarBarco(Cuadricula* cuadricula, Barco* barco) {
 int disparar(Cuadricula* cuadricula_disparo, Cuadricula* cuadricula_barco, Barco barcos[], char* coordenadas, int turno) {
     int fila, columna;
 
-    if (strlen(coordenadas) == 2 || (strlen(coordenadas) == 3 && coordenadas[1] == '0')) {
+    if (strlen(coordenadas) == 2 || (strlen(coordenadas) == 3 && coordenadas[2] == '0')) {
         if (coordenadas[0] >= 'A' && coordenadas[0] <= 'J') {
             if (coordenadas[1] >= '1' && ((coordenadas[2] == '\0') || (coordenadas[1] == '1' && coordenadas[2] == '0'))) {
                 fila = (coordenadas[1] == '1' && coordenadas[2] == '0') ? 9 : coordenadas[1] - '1';
@@ -139,7 +138,7 @@ int disparar(Cuadricula* cuadricula_disparo, Cuadricula* cuadricula_barco, Barco
             return -1; // Indicar que las coordenadas no son válidas
         }
     } else {
-        printf("Coordenadas no válidas. Inténtalo de nuevo.\n");
+        printf("Coordenadas no válidas. Inténtalo de nuevo.lele\n");
         return -1; // Indicar que las coordenadas no son válidas
     }
 
@@ -155,13 +154,28 @@ int disparar(Cuadricula* cuadricula_disparo, Cuadricula* cuadricula_barco, Barco
         cuadricula_barco->tablero[fila][columna] = 'O';    // Barco golpeado
 
         // Reducir la salud del barco golpeado
-        for (int i = 0; i < 5; i++) {
-            if (barcos[i].simbolo == 'B' && barcos[i].coordenada->coordenada[fila][columna] == fila,columna) {
-                barcos[i].salud--;
-                if (barcos[i].salud == 0) {
-                    printf("¡Barco hundido! Coordenadas: %c%d\n", coordenadas[0], fila + 1);
+        
+        for(int j = 0; j < FILAS; j++){
+            for(int k = 0; k < COLUMNAS; k++){
+                for (int i = 0; i < 5; i++) {
+                    if(cuadricula_barco->tablero[j][k] == cuadricula_barco->tablero[fila][columna] && cuadricula_barco->idBarco[j][k]==barcos[i].idBarco){
+                        barcos[i].salud--;
+                        printf("id:%d  salud:%d\n",barcos[i].idBarco,barcos[i].salud);
+                        if (barcos[i].salud == 0) {
+                            printf("¡Barco hundido! Coordenadas: %c%d\n", coordenadas[0], fila + 1);
+                            break;
+                        }
+                        break;
+                    }
                 }
             }
+            
+            // if (barcos[i].simbolo == 'B') {
+            //     barcos[i].salud--;
+            //     if (barcos[i].salud == 0) {
+            //         printf("¡Barco hundido! Coordenadas: %c%d\n", coordenadas[0], fila + 1);
+            //     }
+            // }
         }
 
         return 1;
@@ -184,6 +198,36 @@ void mostrarEstadisticas(EstadisticasJugador jugador1, EstadisticasJugador jugad
     printf("Disparos fallidos: %d\n", jugador2.fallos);
 }
 
+bool comprobarGanador(Cuadricula* cuadricula_barco,Barco barcos[]){
+    
+    //numero total de casillas ocupadas por barcos
+    int total = 0;
+    for(int i=0;i<5;i++)
+    {
+        total += barcos[i].tamano;
+    }
+
+    for(int i=0;i<FILAS;i++)
+    {
+        for(int j=0;j<COLUMNAS;j++)
+        {
+            if(cuadricula_barco->tablero[i][j] == 'O')
+            {
+                total--;
+            }
+        }
+    }
+
+    if(total == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main() {
     // Inicializar la generación de números aleatorios
     srand(time(NULL));
@@ -198,7 +242,8 @@ int main() {
         switch (opcion) {
             case 1:
                 // Crear los barcos, cuadrículas y jugar
-                Barco barcos[] = {{4, 'B', 0,{0}}, {3, 'B', 0,{0}}, {3, 'B', 0,{0}}, {2, 'B', 0,{0}}, {2, 'B', 0,{0}}};
+                Barco barcos1[] = {{4, 'B', 0,1}, {3, 'B', 0,2}, {3, 'B', 0,3}, {2, 'B', 0,4}, {2, 'B', 0,5}};
+                Barco barcos2[] = {{4, 'B', 0,1}, {3, 'B', 0,2}, {3, 'B', 0,3}, {2, 'B', 0,4}, {2, 'B', 0,5}};
                 
                 // Crear las cuadrículas para dos jugadores: barcos y disparos
                 Cuadricula jugador1_barcos, jugador2_barcos, jugador1_disparos, jugador2_disparos;
@@ -212,8 +257,8 @@ int main() {
 
                 // Colocar los barcos en las cuadrículas
                 for (int i = 0; i < 5; i++) {
-                    colocarBarco(&jugador1_barcos, &barcos[i]);
-                    colocarBarco(&jugador2_barcos, &barcos[i]);
+                    colocarBarco(&jugador1_barcos, &barcos1[i]);
+                    colocarBarco(&jugador2_barcos, &barcos2[i]);
                 }
 
                 // Juego: alternar los turnos de los jugadores
@@ -247,14 +292,28 @@ int main() {
                     printf("Tu cuadrícula de disparos:\n");
                     imprimirCuadricula(cuadricula_disparos_propia);
 
+                    Barco barcosAux[5];
+                    if(turno==1)
+                    {
+                        for(int i=0;i<5;i++)
+                            barcosAux[i]=barcos1[i];
+                    }
+                    else
+                    {
+                        for(int i=0;i<5;i++)
+                            barcosAux[i]=barcos2[i];
+                    }
+                    
+
                     // Lógica para que el jugador realice un disparo
                     int disparo_exitoso = 0;
+                    int resultado;
                     while (!disparo_exitoso) {
                         char coordenadas[4]; // Incluyendo espacio para el carácter nulo
                         printf("Introduce coordenadas para disparar (por ejemplo, A1): ");
                         scanf("%s", coordenadas);
 
-                        int resultado = disparar(cuadricula_disparos_propia, cuadricula_barcos_contrincante, barcos, coordenadas, turno);
+                        resultado = disparar(cuadricula_disparos_propia, cuadricula_barcos_contrincante, barcosAux, coordenadas, turno);
                         if (resultado == 1) {
                             printf("¡Disparo acertado! Coordenadas: %s\n", coordenadas);
                             statspropias->aciertos++;
@@ -271,32 +330,66 @@ int main() {
                         }
                     }
 
-                    // Verificar si el jugador ganó
-                    int hundidos = 1;
-                    for (int i = 0; i < 5; i++) {
-                        Barco barco = barcos[i];
-                        for (int fila = 0; fila < FILAS; fila++) {
-                            for (int columna = 0; columna < COLUMNAS; columna++) {
-                                if (cuadricula_barcos_contrincante->tablero[fila][columna] == barco.simbolo) {
-                                    hundidos = 0;
-                                    break;
-                                }
-                            }
-                            if (!hundidos) {
-                                break;
-                            }
-                        }
-                        if (hundidos) {
-                            printf("¡Barco hundido! Coordenadas: %c%d\n", barco.simbolo, i + 1);
-                            break;
-                        }
+
+                    if(comprobarGanador(cuadricula_barcos_contrincante,barcosAux)==true)
+                    {
+                        ganador = turno;
                     }
 
-                    if (hundidos) {
-                        ganador = turno;
-                    } else {
-                        turno = (turno == 1) ? 2 : 1;
+                    if(turno == 1 && resultado == 1)
+                    {
+                        turno = 1;
                     }
+                    else if(turno == 2 && resultado == 1)
+                    {
+                        turno = 2;
+                    }
+                    else if(turno == 1 && resultado == 0)
+                    {
+                        turno = 2;
+                    }
+                    else if(turno == 2 && resultado == 0)
+                    {
+                        turno = 1;
+                    }
+
+                    if(turno==1)
+                    {
+                        for(int i=0;i<5;i++)
+                            barcos1[i]=barcosAux[i];
+                    }
+                    else
+                    {
+                        for(int i=0;i<5;i++)
+                            barcos2[i]=barcosAux[i];
+                    }
+
+                    // Verificar si el jugador ganó
+                    // int hundidos = 1;
+                    // for (int i = 0; i < 5; i++) {
+                    //     Barco barco = barcos[i];
+                    //     for (int fila = 0; fila < FILAS; fila++) {
+                    //         for (int columna = 0; columna < COLUMNAS; columna++) {
+                    //             if (cuadricula_barcos_contrincante->tablero[fila][columna] == barco.simbolo) {
+                    //                 hundidos = 0;
+                    //                 break;
+                    //             }
+                    //         }
+                    //         if (!hundidos) {
+                    //             break;
+                    //         }
+                    //     }
+                    //     if (hundidos) {
+                    //         printf("¡Barco hundido! Coordenadas: %c%d\n", barco.simbolo, i + 1);
+                    //         break;
+                    //     }
+                    // }
+
+                    // if (hundidos) {
+                    //     ganador = turno;
+                    // } else {
+                    //     turno = (turno == 1) ? 2 : 1;
+                    // }
                 }
 
                 printf("\n¡El Jugador %d ha ganado! ¡Felicidades!\n", ganador);
